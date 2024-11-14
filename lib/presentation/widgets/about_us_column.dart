dc35dc3e704302b520c037bb1d3febedf9c84f99
jmapps <jmappsmuslim@gmail.com>
2024-11-14 21:49:17 +0300

    Added about us bottom sheet column

---
+++ lib/presentation/widgets/about_us_column.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import '../../core/strings/app_strings.dart';
+import '../../core/styles/app_styles.dart';
+import 'about_us_list_tile.dart';
+
+class AboutUsColumn extends StatelessWidget {
+  const AboutUsColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: AppStyles.paddingWithoutTop,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            AppStrings.ourApps,
+            style: AppStyles.mainTextStyle17,
+            textAlign: TextAlign.start,
+          ),
+          const SizedBox(height: 8),
+          AboutUsListTile(
+            title: Platform.isAndroid ? AppStrings.googlePlay : AppStrings.appStore,
+            subTitle: AppStrings.moreOurApps,
+            iconName: Platform.isAndroid ? 'google-play' : 'appstore',
+            link: Platform.isAndroid ? AppStrings.linkGooglePlay : AppStrings.linkAppStore,
+          ),
+          const SizedBox(height: 8),
+          Text(
+            AppStrings.ourSocials,
+            style: AppStyles.mainTextStyle17,
+            textAlign: TextAlign.start,
+          ),
+          const SizedBox(height: 8),
+          AboutUsListTile(
+            title: AppStrings.telegram,
+            subTitle: AppStrings.jmapps,
+            iconName: 'telegram',
+            link: AppStrings.linkTelegram,
+          ),
+          const SizedBox(height: 8),
+          AboutUsListTile(
+            title: AppStrings.instagram,
+            subTitle: AppStrings.devMuslim,
+            iconName: 'instagram',
+            link: AppStrings.linkInstagram,
+          ),
+          const SizedBox(height: 8),
+          AboutUsListTile(
+            title: AppStrings.ummaLife,
+            subTitle: AppStrings.jmapps,
+            iconName: 'ummalife',
+            link: AppStrings.linkUmmaLife,
+          ),
+          const SizedBox(height: 8),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/widgets/about_us_list_tile.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../core/styles/app_styles.dart';
+
+class AboutUsListTile extends StatelessWidget {
+  const AboutUsListTile({
+    super.key,
+    required this.title,
+    required this.subTitle,
+    required this.iconName,
+    required this.link,
+  });
+
+  final String title;
+  final String subTitle;
+  final String iconName;
+  final String link;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () async {
+        await _launchUrl();
+      },
+      shape: AppStyles.mainShape,
+      title: Text(
+        title,
+        style: AppStyles.mainTextStyle17Bold,
+      ),
+      subtitle: Text(
+        subTitle,
+        style: AppStyles.mainTextStyle17,
+      ),
+      leading: Image.asset(
+        'assets/icons/$iconName.png',
+        width: 50,
+        height: 50,
+      ),
+      trailing: Icon(Icons.arrow_forward_ios_rounded),
+    );
+  }
+
+  Future<void> _launchUrl() async {
+    final Uri urlLink = Uri.parse(link);
+    await launchUrl(urlLink);
+  }
+}
